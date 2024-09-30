7b13438f631fb19f80c70fb5abb64dce593601bd
Martinica Andrei-Marian <ni3r4ut0m4t4@gmail.com>
2024-09-30 19:51:50 +0300

    book recommendations finished

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,12 +1,28 @@
 import React from 'react'
 
-const Book = ({ title, description, previewlink, infolink, image }) => {
+const Book = ({ title, description, previewlink, infolink, image, authors, categories }) => {
+
+    let link = infolink
+    if (link === null) {
+        link = previewlink
+    }
+    if (link === null) {
+        link = "#"
+    }
+
     return (
         <div className='book'>
-            <img src={`data:image/jpg;base64,${image}`}></img>
-            <div>
-                <h4>{title}</h4>
+            <div className='book-upper'>
+                <a href={link} className='book-image-link'>
+                    <img src={`data:image/jpg;base64,${image}`}></img>
+                </a>
+                <div className='book-upper-right'>
+                    <a href={link}>{title}</a>
+                    <p className='book-authors'>{authors.join(', ')}</p>
+                    <p className='book-categories'>{categories.join(', ')}</p>
+                </div>
             </div>
+            <p className='book-description'>{description}</p>
         </div>
     )
 }
